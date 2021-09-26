8f48cd75a804f32c48b98e492dae9ac6ebaebd6e
Shaini Dasgupta <hottea.code@gmail.com>
2021-09-26 22:51:41 +0530

    first commit

---
+++ Extracting_color.py
@@ -0,0 +1,15 @@
+'''Extracting color from image'''
+
+# import colorgram
+
+# rgb_colors= []
+# # Extract 30 colors from an image.
+# colors = colorgram.extract('image.jpeg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
